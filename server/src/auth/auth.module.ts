cb8c412a680f5ad00bb8b516d44031930bdce40e
pagiestm <pagiestm@gmail.com>
2025-02-14 14:38:17 +0100

    feat(auth): Refactor authentication module to use ConfigService for JWT secret; remove hardcoded constants

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,21 +1,26 @@
-import { Module } from '@nestjs/common';
-import { jwtConstants } from './constants';
+import { Module, forwardRef } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserModule } from 'src/user/user.module';
-import { JwtModule } from '@nestjs/jwt';
+import { AuthGuard } from './auth.guard';
 
 @Module({
   imports: [
-    UserModule,
-    JwtModule.register({
-      global: true,
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '2h' },
-    })
+    ConfigModule,
+    forwardRef(() => UserModule),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '2h' },
+      }),
+    }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, AuthGuard],
   controllers: [AuthController],
-  exports: [AuthService],
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaService } from 'src/prisma.service';
 import { RolesGuard } from 'src/role/roles.guard';
 import { APP_GUARD } from '@nestjs/core';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [forwardRef(() => AuthModule)],
   controllers: [UserController],
   providers: [
     UserService, 
