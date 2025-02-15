ba48cb13237a5e8e0ca90ff3c904ffb40bffea27
pagiestm <pagiestm@gmail.com>
2025-02-15 21:01:12 +0100

    feat(Navbar): Enhance mobile navigation with slide transition and improve button styles

--- client/src/components/Home/Hero.vue
+++ client/src/components/Home/Hero.vue
@@ -2,7 +2,7 @@
     <section
         class="w-full h-screen flex flex-col items-center justify-center text-center bg-gradient-to-b from-primary to-blue-600 text-white px-6">
         <h1 class="text-6xl font-extrabold leading-tight mb-6">
-            Améliorez votre anglais avec <span class="text-secondary">Lingua<span class="animate-bounce md:absolute">Go</span></span>
+            Améliorez votre anglais avec <br /><span class="text-secondary">Lingua<span class="animate-bounce md:absolute">Go</span></span>
         </h1>
         <p class="text-lg text-gray-200 mb-8">
             Une méthode interactive et ludique pour apprendre l’anglais efficacement.
