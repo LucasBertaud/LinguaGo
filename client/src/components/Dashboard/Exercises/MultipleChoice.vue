49a3ba96c2d3dee71bf7ca728afcf7c7c21afe01
Lucas Bertaud <l.bertaud@hotmail.com>
2025-02-18 21:01:02 +0100

    refactor(exercises): refactor exercises by type of exercise and summary.

---
+++ client/src/components/Dashboard/Exercises/MultipleChoice.vue
@@ -0,0 +1,21 @@
+<template>
+    <div v-for="choice in currentExercise.choices" :key="choice">
+        <input type="radio" :id="choice" :value="choice" :checked="choice === userAnswer" @change="updateAnswer(choice)" />
+        <label :for="choice" class="ml-2">{{ choice }}</label>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type Exercise from '../../../interface/exercise.interface';
+
+defineProps<{ 
+    currentExercise: Exercise,
+    userAnswer: string | null 
+}>();
+
+const emit = defineEmits(['update:userAnswer']);
+
+const updateAnswer = (choice: string) => {
+    emit('update:userAnswer', choice);
+};
+</script>
---
+++ client/src/components/Dashboard/Exercises/TrueFalse.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <input type="radio" id="true" value="True" v-model="localAnswer" @input="updateAnswer" />
+        <label for="true" class="ml-2">True</label>
+    </div>
+    <div>
+        <input type="radio" id="false" value="False" v-model="localAnswer" @input="updateAnswer" />
+        <label for="false" class="ml-2">False</label>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const emit = defineEmits(['update:userAnswer']);
+
+const localAnswer = ref('');
+
+const updateAnswer = () => {
+    emit('update:userAnswer', localAnswer.value);
+};</script>
