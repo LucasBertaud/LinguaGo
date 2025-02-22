39efb13262c537595dcecf18d5f150f0822f5a01
Théotime PAGIES <tpagies@ailoop.io>
2025-02-22 10:48:51 +0100

    feat(dashboard): enhance user stats display and improve loading spinner style

--- client/src/components/Dashboard/Stats/TopUsersOfWeekStats.vue
+++ client/src/components/Dashboard/Stats/TopUsersOfWeekStats.vue
@@ -1,18 +1,23 @@
 <template>
-    <div class="row-span-3 bg-white shadow rounded-lg">
+    <div class="flex flex-col row-span-3 bg-white shadow rounded-lg">
         <div class="flex items-center justify-between px-6 py-5 font-semibold border-b border-gray-100">
             <p>Top utilisateurs de la semaine</p>
         </div>
-        <div class="overflow-y-auto m-h-96" v-if="topUsersOfWeek">
-            <ul class="p-6 space-y-6">
-                <li class="flex items-center" v-for="user in topUsersOfWeek" :key="user.pseudo">
-                    <div class="h-10 w-10 mr-3 bg-gray-100 rounded-full overflow-hidden">
-                        <img src="https://randomuser.me/api/portraits/women/82.jpg" alt="Annette Watson profile picture">
-                    </div>
-                    <p class="text-gray-600">{{ user.pseudo }}</p>
-                    <p class="ml-auto font-semibold">{{ user.pointsWon }} pts</p>
-                </li>
-            </ul>
+        <div class="p-4 flex-grow">
+            <div class="overflow-y-auto" v-if="topUsersOfWeek && topUsersOfWeek.length > 0">
+                <ul class="space-y-6">
+                    <li class="flex items-center" v-for="user in topUsersOfWeek" :key="user.pseudo">
+                        <div class="h-10 w-10 mr-3 bg-gray-100 rounded-full overflow-hidden">
+                            <img src="https://randomuser.me/api/portraits/women/82.jpg" alt="Annette Watson profile picture">
+                        </div>
+                        <p class="text-gray-600">{{ user.pseudo }}</p>
+                        <p class="ml-auto font-semibold">{{ user.pointsWon }} pts</p>
+                    </li>
+                </ul>
+            </div>
+            <p v-else class="text-center italic h-full content-center">
+                Aucun utilisateur n'a gagné de points cette semaine
+            </p>
         </div>
     </div>
 </template>
